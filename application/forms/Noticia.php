74caa825b83c08b243e4d04851410f76646f68ae
Marcelo Lopes <marcelomaialopes@yahoo.com.br>
2012-09-01 10:18:13 -0300

    Envio do formulario de cadastro de noticias

---
+++ application/forms/Noticia.php
@@ -0,0 +1,51 @@
+<?php
+
+class Application_Form_Noticia extends Zend_Form
+{
+
+    public function init()
+    {
+        
+        $element = new Zend_Form_Element_Text('titulo');
+        $element->setAttrib('class', 'input-xxlarge')
+                ->setAttrib('placeholder', 'Informe o titulo de sua noticia')
+                ->setRequired(true)
+                 ->addValidator(new Zend_Validate_StringLength(array ('max' => 30))) 
+                ->setAllowEmpty(false);
+        
+        $this->addElement($element);
+        
+          $element = new Zend_Form_Element_Select('categoria');
+        $element->setAttrib('class', 'input-large');      
+        $element->addMultiOptions(array('universidade'=>'Universidade',
+                                        'cidade'=>'Cidade',
+                                        'politica'=>'Política',
+                                        'entretenimento'=>'Entretenimento',
+                                        'outros'=>'Outros'
+       ));
+         $this->addElement($element);
+         
+         $element = new Zend_Form_Element_File('file');
+        //$element->setDestination(MEDIA_PATH);
+        $element->addValidator('Count', false, 1);
+        $element->addValidator('Extension', false, 'jpg,png,gif')
+                ->setDescription("Envie uma foto para a notícia se disponível. Formatos permitidos: jpg, png e gif.");
+        $this->addElement($element);
+        
+            $element = new Zend_Form_Element_Textarea('descricao');
+        $element->setAttrib('style', 'width: 400px')
+                ->setAttrib('rows','12')
+                 ->setAttrib('placeholder', 'Descreva aqui sua notícia')
+                ->addFilter(new Zend_Filter_StripTags());
+        $this->addElement($element);
+        
+        $element = new Zend_Form_Element_Submit('cadastrar');
+        $element->setLabel('Cadastrar notícia')
+                ->setAttrib('class', 'btn btn-large btn')
+                ->setAttrib('style', 'margin-left: 130px');
+        $this->addElement($element);
+    }
+
+
+}
+
